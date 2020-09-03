de0a2f8307f1ef7607d1f0b195655bd8753dd9d7
claudiovivaldi12 <claudiovivaldi389@gmail.com>
2020-09-03 23:23:15 +0700

    changed on sign in

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
 #here is jQuery
+
+function onSignIn(googleUser) {
+  // Useful data for your client-side scripts:
+  let profile = googleUser.getBasicProfile();
+  console.log("ID: " + profile.getId()); // Don't send this directly to your server!
+  console.log('Full Name: ' + profile.getName());
+  console.log('Given Name: ' + profile.getGivenName());
+  console.log('Family Name: ' + profile.getFamilyName());
+  console.log("Image URL: " + profile.getImageUrl());
+  console.log("Email: " + profile.getEmail());
+
+  // The ID token you need to pass to your backend:
+  let id_token = googleUser.getAuthResponse().id_token;
+  console.log("ID Token: " + id_token);
+  // $.ajax({
+  // method: 'POST',
+  // url: 'http://localhost:58155/tokensignin',    //ini url backend kita, nanti gw bikin endpoint tokensignin
+  // data: { id_token: id_token }
+  // })
+  // .done((response)=>{
+  //
+  // })
+  // .fail((jqXHR, textStatus)=>{
+  //
+  // })
+}
